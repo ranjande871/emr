4d31d0bf6ecb5b03e6571b0cb8b3098a51b35e7e
deepak <deepakranjanmt@outlook.com>
2026-02-07 10:35:53 +0530

    added now

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -1,31 +1,36 @@
-module "emr_serverless" {
-  source = "../../modules/emr-serverless-app"
+resource "aws_cloudwatch_log_group" "this" {
+  count             = var.enable_cloudwatch_logs ? 1 : 0
+  name              = "/aws/emr-serverless/${var.name}"
+  retention_in_days = var.log_retention_days
+}
 
+resource "aws_emrserverless_application" "this" {
   name          = var.name
   release_label = var.release_label
   type          = var.type
 
-  s3_bucket = var.s3_bucket
-  s3_prefix = var.s3_prefix
+  # NOTE:
+  # Removing initial_capacity avoids consuming concurrent vCPU quota during
+  # application creation. Capacity will be allocated when jobs are submitted.
+  # If you later increase quota, you can re-add initial_capacity.
 
-  enable_cloudwatch_logs = var.enable_cloudwatch_logs
-  log_retention_days     = var.log_retention_days
+  maximum_capacity {
+    cpu    = var.max_cpu
+    memory = var.max_memory
+  }
 
-  driver_worker_count   = var.driver_worker_count
-  executor_worker_count = var.executor_worker_count
+  # NOTE:
+  # Your current AWS provider schema does NOT support application-level
+  # monitoring_configuration { cloud_watch_monitoring_configuration { ... } }.
+  # We'll configure CloudWatch logging at the JOB RUN level later (or upgrade provider).
 
-  driver_cpu    = var.driver_cpu
-  driver_memory = var.driver_memory
-
-  executor_cpu    = var.executor_cpu
-  executor_memory = var.executor_memory
-
-  max_cpu    = var.max_cpu
-  max_memory = var.max_memory
-
-  enable_vpc         = var.enable_vpc
-  subnet_ids         = var.subnet_ids
-  security_group_ids = var.security_group_ids
+  dynamic "network_configuration" {
+    for_each = var.enable_vpc ? [1] : []
+    content {
+      subnet_ids         = var.subnet_ids
+      security_group_ids = var.security_group_ids
+    }
+  }
 
   tags = var.tags
 }
